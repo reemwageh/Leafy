6f68c77cde05bea12c8835591abf758ec7d5e555
reemwageh <reemwageh771@gmail.com>
2024-06-30 20:20:49 +0300

    get userType by id

--- src/main/java/com/example/graduationProject/Controller/UserTypeController.java
+++ src/main/java/com/example/graduationProject/Controller/UserTypeController.java
@@ -4,17 +4,26 @@ import com.example.graduationProject.Entity.UserType;
 import com.example.graduationProject.Service.UserTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
+
 @RestController
 public class UserTypeController {
     @Autowired
     private UserTypeService userTypeService;
     @GetMapping("/get/userType/all")
-    public List<UserType> userTypeList(){
+    public List<UserType> userTypeList() {
         return userTypeService.fetchAllUserTypes();
     }
 
+        @GetMapping("/get/userType/{typeId}")
+        public Optional<UserType> getUserById(@PathVariable ("typeId") int typeId){
+            return userTypeService.getUserTypeById(typeId);
+        }
+    }
 
-}
+
+
